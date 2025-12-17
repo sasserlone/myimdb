0e79b38f45304a196ca51a451aaa9e780fdc8de6
sasserlone <sasserlone@mailbox.org>
2025-12-17 07:23:48 +0100

    logs entfernt

--- logs/run_pipeline_6941a7ff143eb.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-"C:\xampp\php\php.exe" "C:\xampp\htdocs\movies\scripts\pipeline_runner.php" --episodes="C:\Users\User\Downloads\imdb\imdb\title.episode.tsv" --principals="C:\Users\User\Downloads\imdb\imdb\title.principals.tsv" --names="C:\Users\User\Downloads\imdb\imdb\name.basics.tsv" --log="C:\xampp\htdocs\movies\logs\imdb_pipeline_20251216_194207.log"
